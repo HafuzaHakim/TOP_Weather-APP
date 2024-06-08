b8c9518286b067adf6117e6efc896ff090fc766d
HafuzaHakim <hafuzahakim@gmail.com>
2024-06-08 09:23:11 +0800

    Added input form and page styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-const straw = 'this is a string';
+import '../src/style.css';
+import { getData } from './modules/fetch';
+
+getData('Kampar')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.error('Error outside', err));
+
+function renderData(data) {}
+
+function displayError() {}
---
+++ src/modules/fetch.js
@@ -0,0 +1,55 @@
+const apiKey = '6f3394a19a3a46478a301618240706';
+// API Key should always be secured and never to be exposed to the public. However for the purpose of creating this application, there should be no harm to expose the key at the client sied due to the nature of the key being free to the public.
+const url = 'http://api.weatherapi.com/v1';
+
+export async function getData(city) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7`
+    );
+    const data = await response.json();
+    if (!response.ok) {
+      console.log(data);
+      switch (true) {
+        case data.error.code === 1002:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 1003:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 1005:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 1006:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 2006:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 2007:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 2008:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 2009:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 9000:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 9001:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        case data.error.code === 9999:
+          throw new Error(`Error ${data.error.code} : ${data.error.message}`);
+          break;
+        default:
+          throw new Error('Unknown error');
+      }
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
